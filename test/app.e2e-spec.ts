d3953522398aacc0b82741567c93899c256329d3
Liha Kim <lihakim.kr@gmail.com>
2021-07-22 22:32:39 +0200

    [22/Jul/21] - e2e testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,9 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test, TestingModule } from "@nestjs/testing";
+import { INestApplication } from "@nestjs/common";
+import * as request from "supertest";
+import { AppModule } from "./../src/app.module";
 
-describe('AppController (e2e)', () => {
+describe("AppController (e2e)", () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,10 +15,29 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it("/ (GET)", () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get("/")
       .expect(200)
-      .expect('Hello World!');
+      .expect("Welcome to my Movie API");
+  });
+
+  describe("/movies", () => {
+    it("GET", () => {
+      return request(app.getHttpServer()).get("/movies").expect(200).expect([]);
+    });
+    it("POST", () => {
+      return request(app.getHttpServer())
+        .post("/movies")
+        .send({
+          title: "test title",
+          year: 2021,
+          genres: ["test genre"],
+        })
+        .expect(201);
+    });
+    it("DELETE", () => {
+      return request(app.getHttpServer()).delete("/movies").expect(404);
+    });
   });
 });
